54298fae4c15490a65ecc7c14b0064371291e5fe
Atilla Yadik <atillayadik@gmail.com>
2022-11-08 00:50:30 -0600

    Add project files.

---
+++ MySort/BenchmarkClass.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySort
+{
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class BenchmarkClass
+    {
+        private int[] intcol;
+        [GlobalSetup]
+        public void Setup()
+        { 
+            intcol = Enumerable.Range(0, 1000).Select(x => Random.Shared.Next(1, 10000)).ToArray();
+        }
+
+        [Benchmark]
+        public void GetAtillaSort()
+        {
+            var sorted = AtillaSort.SortAndRemoveDuplicates(intcol);
+        }
+
+        [Benchmark]
+        public int[] OrderBy() => intcol.OrderBy(x => x).ToArray();
+
+
+        [Benchmark]
+        public int[] OrderByDesc() => intcol.OrderByDescending(x => x).ToArray();
+
+
+    }
+}
---
+++ MySort/MySort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySort
+{
+    public static class AtillaSort
+    {
+        public static Int64[] SortAndRemoveDuplicates(int[] data)
+        {
+            var tmp1 = new int[10000];
+           
+            for (var i = 0; i < data.Length; i++)
+            {
+                int currentVal = data[i];
+                tmp1[currentVal] = currentVal;
+            }
+
+            var tmp2 = new List<Int64>();
+
+            tmp2.Add(tmp1[0]);
+            for (var i = 1; i < tmp1.Length; ++i)
+            {
+                var tmp = tmp1[i];
+                if (tmp > 0)
+                    tmp2.Add(tmp) ;
+            }
+
+            return tmp2.ToArray<Int64>();
+
+        }
+    }
+}
