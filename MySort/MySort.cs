bd86589f3f1899619790040d3d0d9495b9380120
AtillaYadik <atillayadik@gmail.com>
2025-12-17 22:23:57 -0600

    Refactor SortAndRemoveDuplicates method
    Refactor SortAndRemoveDuplicates to handle null and empty inputs, optimize duplicate removal, and improve variable naming.

--- MySort/MySort.cs
+++ MySort/MySort.cs
@@ -9,28 +9,48 @@ namespace MySort
 {
     public static class AtillaSort
     {
-        public static Int64[] SortAndRemoveDuplicates(int[] data)
+        public static Int64[] SortAndRemoveDuplicates(int[] numbers)
         {
-            var tmp1 = new int[10000];
-           
-            for (var i = 0; i < data.Length; i++)
-            {
-                int currentVal = data[i];
-                tmp1[currentVal] = currentVal;
-            }
+            if (numbers == null) throw new ArgumentNullException(nameof(numbers));
+        if (numbers.Length == 0) return Array.Empty<int>();
 
-            var tmp2 = new List<Int64>();
+        int min = int.MaxValue;
+        int max = int.MinValue;
 
-            tmp2.Add(tmp1[0]);
-            for (var i = 1; i < tmp1.Length; ++i)
-            {
-                var tmp = tmp1[i];
-                if (tmp > 0)
-                    tmp2.Add(tmp) ;
-            }
-
-            return tmp2.ToArray<Int64>();
-
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            int v = numbers[i];
+            if (v < min) min = v;
+            if (v > max) max = v;
         }
+
+        long rangeLong = (long)max - min + 1;
+        if (rangeLong > int.MaxValue)
+            throw new InvalidOperationException($"Range too large: {rangeLong}.");
+
+        int range = (int)rangeLong;
+        var present = new bool[range];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            present[numbers[i] - min] = true;
+        }
+
+        // Count uniques
+        int count = 0;
+        for (int i = 0; i < range; i++)
+        {
+            if (present[i]) count++;
+        }
+
+        // Fill output
+        var output = new int[count];
+        int idx = 0;
+        for (int i = 0; i < range; i++)
+        {
+            if (present[i]) output[idx++] = i + min;
+        }
+
+        return output;
     }
 }
