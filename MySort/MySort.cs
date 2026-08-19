08a864f828aa1fc4d2b20a6e5a36858e3f1aed94
AtillaYadik <atillayadik@gmail.com>
2026-08-19 09:43:29 -0500

    Refactor sorting method to remove duplicates

--- MySort/MySort.cs
+++ MySort/MySort.cs
@@ -9,28 +9,51 @@ namespace MySort
 {
     public static class AtillaSort
     {
-       public static Int64[] SortAndRemoveDuplicates(int[] data)
+      public static int[] AtillaNoDuplicateSorting(int[] arr)
+    {
+        if (arr == null || arr.Length == 0) return Array.Empty<int>();
+
+        // In C#, the array is already typed as int[], so there's no need
+        // for a runtime "is it an integer" check like in JS (where numbers
+        // can be floats). If your input might come in as double[] or object[],
+        // let me know and I can add that validation back in.
+
+        int min = int.MaxValue;
+        int max = int.MinValue;
+
+        foreach (var v in arr)
         {
-            var tmp1 = new int[10000];
-           
-            for (var i = 0; i < data.Length; i++)
-            {
-                int currentVal = data[i];
-                tmp1[currentVal] = currentVal;
-            }
-
-            var tmp2 = new List<Int64>();
-
-            tmp2.Add(tmp1[0]);
-            for (var i = 1; i < tmp1.Length; ++i)
-            {
-                var tmp = tmp1[i];
-                if (tmp > 0)
-                    tmp2.Add(tmp) ;
-            }
-
-            return tmp2.ToArray<Int64>();
-
+            if (v < min) min = v;
+            if (v > max) max = v;
         }
+
+        long k = (long)max - min + 1;
+
+        // Bail out to normal sort if range is too big relative to n
+        if (k > arr.Length * 10L)
+        {
+            return arr.Distinct().OrderBy(x => x).ToArray();
+        }
+
+        // Use nullable int as a stand-in for JS's "undefined" sentinel
+        var buckets = new int?[k];
+
+        foreach (var v in arr)
+        {
+            long idx = (long)v - min;
+            buckets[idx] = v; // dedupe
+        }
+
+        var result = new List<int>();
+        for (long i = 0; i < k; i++)
+        {
+            if (buckets[i].HasValue)
+            {
+                result.Add(buckets[i].Value);
+            }
+        }
+
+        return result.ToArray();
+    }
     }
 }
